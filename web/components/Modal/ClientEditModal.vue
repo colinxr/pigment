ccf7b8d8ed44707fb66ad5f6514bfd6f19c6fd3b
Colin Rabyniuk <colinxr@gmail.com>
2023-05-24 17:45:04 -0700

    set up alert components

--- web/components/Modal/ClientEditModal.vue
+++ web/components/Modal/ClientEditModal.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="bg-white border border-gray mx-auto w-full max-w-[550px] rounded-xl p-4">
-    <header class="flex space-between">
-      <h1>Update Client</h1>
-      <span class="cursor-pointer" @click="store.closeModal">X</span>
+    <header>
+      <div class="flex space-between">
+        <h1>Update Client</h1>
+        <span class="cursor-pointer" @click="store.closeModal">X</span>
+      </div>
+      <component :is="formAlert" />
     </header>
 
     <DynamicForm :schema="formSchema" :data="props.client" @form-submitted="handleSubmit" />
@@ -14,6 +17,7 @@ import ApiService from '@dayplanner/apiservice'
 import useModalStore from '@/stores/modal'
 import useFormErrors from '@/composables/useFormErrors'
 import DynamicForm from '@/components/Forms/DynamicForm.vue'
+import AlertSuccess from '@/components/Alerts/SuccessAlert.vue'
 
 const { errorState, handleResponseErrors } = useFormErrors()
 
@@ -25,6 +29,9 @@ const props = defineProps({
     required: true,
   },
 })
+
+const formStatus = ref('untouched')
+const formAlert = ref({})
 
 const formSchema = [
   {
@@ -47,20 +54,33 @@ const formSchema = [
   },
 ]
 
+watch(
+  () => formStatus,
+  () => {
+    if (formStatus.value === 'success') { return formAlert.value = AlertSuccess }
+
+    return null
+  }
+)
+
 const handleSubmit = async (formData) => {
+  formStatus.value = 'loading'
   try {
     const res = await ApiService.clients.update(formData)
 
     if (res.status !== 200) {
-      return handleResponseErrors(response)
+      handleResponseErrors(res)
     }
 
     console.log(res)
+
+    formStatus.value = 'success'
     return true
   } catch (error) {
     console.log(error)
     errorState.isSet = true
     errorState.message = 'something went wrong'
+    formStatus.value = 'error'
     return false
   }
 }
