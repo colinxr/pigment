9f521a17701461ab69618d92c110242736016d0a
Colin Rabyniuk <colinxr@gmail.com>
2023-07-14 11:00:07 -0700

    config for pm2 in symlink delpoy step

--- web/ecosystem.config.js
+++ web/ecosystem.config.js
@@ -6,6 +6,9 @@ module.exports = {
 			exec_mode: 'cluster',
 			instances: 'max',
 			script: '.output/server/index.mjs',
+			env: {
+				NUXT_PUBLIC_API_URL: process.env.NUXT_PUBLIC_API_URL,
+			},
 		},
 	],
 }
