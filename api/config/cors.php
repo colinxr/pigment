b0af0ef1ed3c9e03847a25d8b1dddfadd2e23881
Colin Rabyniuk <colinxr@gmail.com>
2023-07-18 17:12:58 -0700

    cors allowed origins

--- api/config/cors.php
+++ api/config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://app.usepigment.com', 'https://app.pigment.biz:3000'],
 
     'allowed_origins_patterns' => [],
 
