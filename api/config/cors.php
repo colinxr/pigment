b4da3c3ae88476abcbdfddbcff41b739aa0d0a5e
Colin Rabyniuk <colinxr@gmail.com>
2023-07-20 13:42:23 -0700

    api gh flow permissions; logging permissions

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://app.usepigment.com'],
+    'allowed_origins' => [env('WEB_URL')],
 
     'allowed_origins_patterns' => [],
 
