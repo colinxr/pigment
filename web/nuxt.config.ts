da81c006ccf18c70677b759574c360585171d09a
Colin Rabyniuk <colinxr@gmail.com>
2023-07-22 20:58:41 -0700

    set up web client to serve over https

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
 
 
-// NUXT_NODE_TLS_REJECT_UNAUTHORIZED=0
+// NUXT_ENV_API_URL=https://api.pigment.biz/api NUXT_NODE_TLS_REJECT_UNAUTHORIZED=0
+
+// NUXT_ENV_API_URL=https://api.pigment.biz/api NUXT_NODE_TLS_REJECT_UNAUTHORIZED=0
+
+    // "dev": "NUXT_ENV_API_URL=https://api.pigment.biz/api NUXT_NODE_TLS_REJECT_UNAUTHORIZED=0 npx nuxi dev --host pigment.biz --https --port 3001 --ssl-cert pigment.biz.pem --ssl-key pigment.biz-key.pem",
