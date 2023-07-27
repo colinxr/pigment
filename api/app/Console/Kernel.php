4614721341b731377bfe534aed1997ae4d9fd510
Colin Rabyniuk <colinxr@gmail.com>
2023-07-26 22:28:22 -0700

    add github action to backup database

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -13,8 +13,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run')->daily()->at('01:30');
+        if (app()->environment('production')) {
+            $schedule->command('backup:clean')->daily()->at('01:00');
+            $schedule->command('backup:run')->daily()->at('01:30');
+        }
     }
 
     /**
