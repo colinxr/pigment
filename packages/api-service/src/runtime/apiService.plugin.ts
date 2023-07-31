47923ec38cc81e7c6ec6b0213f29658bec3e3f10
Colin Rabyniuk <colinxr@gmail.com>
2023-07-31 14:57:21 -0700

    debug apiservice config

--- packages/api-service/src/runtime/apiService.plugin.ts
+++ packages/api-service/src/runtime/apiService.plugin.ts
@@ -2,7 +2,10 @@ import { defineNuxtPlugin, useRuntimeConfig } from '#app'
 import ApiService from './apiService'
 
 export default defineNuxtPlugin(() => {
+  console.log(useRuntimeConfig().public);
+  
   const options = useRuntimeConfig().public.apiService
+  console.log(options);
 
   const apiService = new ApiService(options.apiUrl)
 
