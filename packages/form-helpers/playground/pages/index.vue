5d28768e73f857d6fdb73bad817be69367f8c42d
Colin Rabyniuk <colinxr@gmail.com>
2023-07-30 18:11:52 -0700

    check in playground?

---
+++ packages/form-helpers/playground/pages/index.vue
@@ -0,0 +1,91 @@
+<script setup>
+const route = useRoute()
+const { $apiService } = useNuxtApp()
+
+const {
+	errorState,
+	showFormAlert,
+	formStatus,
+	alertMessage,
+	handleResponseErrors,
+} = useFormErrors()
+
+console.log(useFormErrors());
+
+const formSchema = [
+	{
+		$formkit: 'text',
+		label: 'First Name',
+		name: 'first_name',
+		validation: 'required',
+		value: 'test',
+		validationVisibility: 'dirty',
+	},
+	{
+		$formkit: 'text',
+		label: 'Last Name',
+		name: 'last_name',
+		validation: 'required',
+		validationVisibility: 'dirty',
+	},
+
+	{
+		$formkit: 'text',
+		label: 'Email',
+		name: 'email',
+		validation: 'required',
+		validationVisibility: 'dirty',
+	},
+]
+
+const initialValues = {
+	first_name: null,
+	last_name: null,
+	email: null,
+	phone: null,
+	idea: null,
+}
+
+const handleSubmit = async () => {
+	try {
+		const { username } = route.params
+		const res = await $apiService.submissions.store(username, formData)
+
+		if (res.status !== 200) handleResponseErrors(res)
+
+		showFormAlert.value = true
+		formStatus.value = 'success'
+		alertMessage.value = res.data.message || 'Request submitted'
+
+		return
+	} catch (error) {
+		console.log(error)
+
+		if (error.response?.status === 403) return
+
+		alertMessage.value = 'Something went wrong'
+		formStatus.value = 'error'
+		showFormAlert.value = true
+	}
+}
+</script>
+
+<template>
+	<div>
+		<header>
+			<div class="flex justify-between mb-2">
+				<h1>Appointment Submission Form</h1>
+			</div>
+
+			<AlertWrapper v-if="showFormAlert" :status="formStatus" :msg="alertMessage" />
+		</header>
+
+		<DynamicForm
+			formId="client-create"
+			:schema="formSchema"
+			:data="initialValues"
+			:errorState="{}"
+			:disabled="false"
+		/>
+	</div>
+</template>
